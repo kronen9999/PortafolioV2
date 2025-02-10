4e1e8401b72e02e3c0ec1982613189336f168e2b
kronen9999 <stacica22eneronkio@gmail.com>
2025-02-09 20:01:19 -0600

    Se Agrego el apartado de about al protafolio

--- Resources/Js/Elements.js
+++ Resources/Js/Elements.js
@@ -29,5 +29,46 @@
       return ulRedes;
     }
 
+    static ulMenu()
+    {
+      let ulMenu= document.createElement("ul");
+      ulMenu.classList.add("ulMenu");
+      ulMenu.innerHTML=`
+      <li seleccion='true' btnMenu='Home'>Home</li>
+      <li seleccion='false' btnMenu='About'>About</li>
+      <li seleccion='false' btnMenu='AcHistorial'>Academic Historial</li>
+      <li seleccion='false' btnMenu='Projects'>Projects</li>
+      <li seleccion='false' btnMenu='Skills'>Skills</li>
+      <li seleccion='false' btnMenu='Contact'>Contact</li>
+    `;
+    return ulMenu;
+    }
+
+    static ELementoHome()
+    {
+      let divElementoHome=document.createElement("div");
+      divElementoHome.classList.add("ElementoHome");
+      divElementoHome.innerHTML=`
+      <h1 class='Home_Title'>Hi i am steven </h1>
+      <h2 class='Home_Description'>Welcome to my portfolio</h2>
+    `;
+    return divElementoHome;
+    }
+
+    static ELementoAbout()
+    {
+      let divElementoVariado=document.createElement("div");
+      divElementoVariado.classList.add("divElementoVariado");
+      divElementoVariado.innerHTML=`<h2 class='InfoApartado'>
+      About me
+      </h2>
+      <div class='AboutMe_Div'>
+      <p>Hi,My name is Steven Camacho Cisneros.Just now i am a
+       student of the informatics Engineering career in the Instituto
+       Tecnologico Superior of Acatlan de Osorio </p>
+      </div>
+    `;
+    return divElementoVariado;
+    }
 
  }
--- Resources/Js/index.js
+++ Resources/Js/index.js
@@ -1,6 +1,8 @@
 import { Elements as elements } from './Elements.js';
-
+    var apartadoMenu="Home"; 
 document.addEventListener('DOMContentLoaded', function() {
+
+  
 
     const divPrincipal= this.querySelector(".div_Principal");
     divPrincipal.appendChild(elements.WelcomeMessage());
@@ -9,18 +11,82 @@ document.addEventListener('DOMContentLoaded', function() {
     divPrincipal.removeChild(divPrincipal.querySelector(".WelcomeMessage"));
     divPrincipal.appendChild(elements.divContenedor());
     document.querySelector(".divContenedor").innerHTML=`<div class='divContenedor_P1'>
-    <h2>Found me on</h2>
+    <h2>You can found me on</h2>
     </div>
     <div class='divContenedor_P2'>
+    <div class='divInfo'>
+    </div>
+    <div class='divMenu'>
+    </div>
     </div>
     `;
     document.querySelector(".divContenedor_P1").appendChild(elements.ulRedes());
+    document.querySelector(".divInfo").appendChild(elements.ELementoHome());
+    document.querySelector(".divMenu").appendChild(elements.ulMenu());
+    const elementosMenu=document.querySelectorAll(".ulMenu li");
+    MenuSeleccion(elementosMenu);
+    
+    
+
+
     },4500);
-
-
-
-    
-    
-    
-
 });
+
+function MenuSeleccion (elementosMenu)
+{
+    elementosMenu.forEach(elemento => {
+        elemento.addEventListener("click",(e)=>{
+        for ( let i of elementosMenu)
+        {
+        if (i == e.target)
+        {
+            i.setAttribute("seleccion","true");
+            apartadoMenu=i.getAttribute("btnMenu");
+        }
+        else {
+            i.setAttribute("seleccion","false");
+        }
+
+
+        }
+    SeleccionMenu(apartadoMenu);
+        
+    });
+    
+        });  
+
+}
+
+function SeleccionMenu(apartadoMenu)
+{
+
+    let divInfo=document.querySelector(".divInfo");
+
+    divInfo.innerHTML="";
+    switch(apartadoMenu)
+    {
+        case "Home":
+            divInfo.appendChild(elements.ELementoHome());
+            break;
+        case "About":
+            divInfo.appendChild(elements.ELementoAbout());
+            break;
+        case "AcHistorial":
+            alert("AcHistorial");
+            break;
+        case "Projects":
+            alert("Projects");
+            break;
+        case "Skills":
+            alert("Skills");
+            break;
+        case "Contact":
+            alert("Contact");
+            break;
+    }
+
+}
+
+
+
+
